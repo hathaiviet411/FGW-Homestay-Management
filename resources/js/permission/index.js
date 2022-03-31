d6250df26f7a3bd1d36504768e0147368de1b5b0
Ha Thai Viet <hathaiviet411@gmail.com>
2022-03-31 10:27:00 +0700

    [FE] - Inital Base.

--- resources/js/permission/index.js
+++
@@ -1,8 +0,0 @@
-import router from '@/router';
-import getPageTitle from '@/utils/getPageTitle';
-
-router.beforeEach(async (to, from, next) => {
-	document.title = getPageTitle(to.meta.title);
-
-	next();
-});
--- resources/js/store/getters.js
+++
@@ -1,5 +0,0 @@
-const getters = {
-	language: state => state.app.language
-};
-
-export default getters;
